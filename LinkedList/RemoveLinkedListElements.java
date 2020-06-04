3023dfeffa060fc8b85ad55e558eff76ae071a95
TaoOneHacker <taoonehacker@gmail.com>
2020-06-04 13:56:07 +0800

    原地算法删除数组重复元素

--- LinkedList/RemoveLinkedListElements.java
+++ LinkedList/RemoveLinkedListElements.java
@@ -40,11 +40,12 @@ public class RemoveLinkedListElements {
         }
         return head;
     }
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){val=x;}
+    }
+
 }
 
-public class ListNode{
-    int val;
-    ListNode next;
-    ListNode(int x){val=x;}
-}
 
